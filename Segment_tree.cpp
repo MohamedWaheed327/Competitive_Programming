a64fce54dc79b66f723a0d80f11213a89df9c9ff
MohamedWaheed327 <145778227+MohamedWaheed327@users.noreply.github.com>
2023-09-28 14:05:42 +0300

    Update Segment_tree.cpp

--- Segment_tree.cpp
+++ Segment_tree.cpp
@@ -5,12 +5,41 @@ using namespace std;
 
 struct segment_tree // 1 index
 {
-    ll size = 1, SKIP = 0;
-    vector<ll> seg, buildseg;
-
-    ll merge(ll a, ll b)
+private:
+    struct node
     {
-        return a + b;
+        ll mn = LLONG_MAX, mn_num = 1, gc = 0;
+        node() {}
+        node(ll x)
+        {
+            mn = x;
+            gc = x;
+        }
+    };
+
+    ll size = 1;
+    node SKIP;
+    vector<node> seg;
+    vector<ll> buildseg;
+
+    node merge(node a, node b)
+    {
+        node ret;
+        if (a.mn == b.mn)
+        {
+            ret.mn = a.mn;
+            ret.mn_num = a.mn_num + b.mn_num;
+        }
+        else if (a.mn < b.mn)
+        {
+            ret = a;
+        }
+        else
+        {
+            ret = b;
+        }
+        ret.gc = gcd(a.gc, b.gc);
+        return ret;
     }
 
     void build(ll x, ll lx, ll rx)
@@ -18,7 +47,7 @@ struct segment_tree // 1 index
         if (rx - lx == 1)
         {
             if (lx < buildseg.size())
-                seg[x] = buildseg[lx];
+                seg[x] = node(buildseg[lx]);
             return;
         }
         ll m = (lx + rx) / 2;
@@ -27,20 +56,11 @@ struct segment_tree // 1 index
         seg[x] = merge(seg[2 * x + 1], seg[2 * x + 2]);
     }
 
-    segment_tree(vector<ll> a)
-    {
-        buildseg = a;
-        while (size < a.size())
-            size *= 2;
-        seg.assign(2 * size, SKIP);
-        build(0, 0, size);
-    }
-
     void update(ll ind, ll value, ll x, ll lx, ll rx)
     {
         if (rx - lx == 1)
         {
-            seg[x] = value;
+            seg[x] = node(value);
             return;
         }
         ll m = (lx + rx) / 2;
@@ -51,25 +71,35 @@ struct segment_tree // 1 index
         seg[x] = merge(seg[2 * x + 1], seg[2 * x + 2]);
     }
 
-    void update(ll ind, ll value)
-    {
-        update(--ind, value, 0, 0, size);
-        buildseg[ind] = value;
-    }
-
-    ll query(ll l, ll r, ll x, ll lx, ll rx)
+    node query(ll l, ll r, ll x, ll lx, ll rx)
     {
         if (l > rx || r < lx)
             return SKIP;
         if (l <= lx && rx <= r)
             return seg[x];
         ll m = (lx + rx) / 2;
-        ll s1 = query(l, r, 2 * x + 1, lx, m);
-        ll s2 = query(l, r, 2 * x + 2, m + 1, rx);
+        node s1 = query(l, r, 2 * x + 1, lx, m);
+        node s2 = query(l, r, 2 * x + 2, m + 1, rx);
         return merge(s1, s2);
     }
 
-    ll query(ll l, ll r)
+public:
+    segment_tree(vector<ll> a)
+    {
+        buildseg = a;
+        while (size < a.size())
+            size *= 2;
+        seg.assign(2 * size, SKIP);
+        build(0, 0, size);
+    }
+
+    void update(ll ind, ll value)
+    {
+        update(--ind, value, 0, 0, size);
+        buildseg[ind] = value;
+    }
+
+    node query(ll l, ll r)
     {
         return query(--l, --r, 0, 0, size - 1);
     }
@@ -89,24 +119,16 @@ void Main()
     cin >> q;
     while (q--)
     {
-        ll op;
-        cin >> op;
-        if (op == 1)
-        {
-            ll indx, val;
-            cin >> indx >> val;
-            st.update(indx, val);
-        }
+        ll l, r;
+        cin >> l >> r;
+        auto a = st.query(l, r);
+        if (a.gc == a.mn)
+            cout << r - l + 1 - a.mn_num << '\n';
         else
-        {
-            ll l, r;
-            cin >> l >> r;
-            cout << st.query(l, r) << '\n';
-        }
+            cout << r - l + 1 << '\n';
     }
 }
 /*
-
 
 
 
