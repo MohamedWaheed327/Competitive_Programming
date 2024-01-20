14b964e29cb4eefa7a915f3cfc8fd8c50ccdf67b
MohamedWaheed327 <145778227+MohamedWaheed327@users.noreply.github.com>
2024-01-20 23:01:38 +0200

    Update Segment_tree.cpp

--- Segment_tree.cpp
+++ Segment_tree.cpp
@@ -1,130 +1,141 @@
-// Mohamed_Waheed_
+// Mohamed_Waheed
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
 
-template <class T>
-class segment_tree
-{
+class segment_tree {
 private:
-    struct node
-    {
-        ll skip = 0;
-        node() { skip = 1; }
+    struct node {
 
-        node(T x)
-        {
+        void apply(int lx, int rx, long long val) {
         }
     };
 
-    ll size = 1;
-    vector<node> seg;
-    vector<T> buildseg;
-
-    node merge(node a, node b)
-    {
-        if (a.skip)
-            return b;
-        if (b.skip)
-            return a;
+    node merge(const node &a, const node &b) const {
         node ret;
-        ret.skip = 0;
 
         return ret;
     }
 
-    void build(ll x, ll lx, ll rx)
-    {
-        if (rx - lx == 0)
-        {
-            if (lx < buildseg.size())
-                seg[x] = node(buildseg[lx]);
-            return;
-        }
-        ll m = (lx + rx) / 2;
-        build(2 * x + 1, lx, m);
-        build(2 * x + 2, m + 1, rx);
-        seg[x] = merge(seg[2 * x + 1], seg[2 * x + 2]);
+    inline void propagate(int x, int lx, int rx) {
+        // int mid = lx + rx >> 1;
+        // int left = x + 1;
+        // int right = x + (mid - lx + 1 << 1);
+        // if (~seg[x].lazy) {
+        //     seg[left].apply(lx, mid, seg[x].lazy);
+        //     seg[right].apply(mid + 1, rx, seg[x].lazy);
+        //     seg[x].lazy = -1;
+        // }
     }
 
-    void update(ll ind, T value, ll x, ll lx, ll rx)
-    {
-        if (rx - lx == 0)
-        {
-            seg[x] = node(value);
+    int size;
+    vector<node> seg;
+
+    template <class... M>
+    void build(int x, int lx, int rx, const vector<M> &...build_seg) {
+        if (lx == rx) {
+            seg[x].apply(lx, rx, build_seg[lx]...);
             return;
         }
-        ll m = (lx + rx) / 2;
-        if (ind <= m)
-            update(ind, value, 2 * x + 1, lx, m);
-        else
-            update(ind, value, 2 * x + 2, m + 1, rx);
-        seg[x] = merge(seg[2 * x + 1], seg[2 * x + 2]);
+        int mid = lx + rx >> 1;
+        int left = x + 1;
+        int right = x + (mid - lx + 1 << 1);
+        build(left, lx, mid, build_seg...);
+        build(right, mid + 1, rx, build_seg...);
+        seg[x] = merge(seg[left], seg[right]);
     }
 
-    node query(ll l, ll r, ll x, ll lx, ll rx)
-    {
-        if (l > rx || r < lx)
-            return node();
+    template <class... M>
+    void update(int x, int lx, int rx, int l, int r, const M &...value) {
+        if (l <= lx && rx <= r) {
+            seg[x].apply(lx, rx, value...);
+            return;
+        }
+        propagate(x, lx, rx);
+        int mid = lx + rx >> 1;
+        int left = x + 1;
+        int right = x + (mid - lx + 1 << 1);
+        if (l <= mid) {
+            update(left, lx, mid, l, r, value...);
+        }
+        if (r >= mid + 1) {
+            update(right, mid + 1, rx, l, r, value...);
+        }
+        seg[x] = merge(seg[left], seg[right]);
+    }
+
+    node query(int x, int lx, int rx, int l, int r) {
         if (l <= lx && rx <= r)
             return seg[x];
-        ll m = (lx + rx) / 2;
-        node s1 = query(l, r, 2 * x + 1, lx, m);
-        node s2 = query(l, r, 2 * x + 2, m + 1, rx);
-        return merge(s1, s2);
+        propagate(x, lx, rx);
+        int mid = lx + rx >> 1;
+        int left = x + 1;
+        int right = x + (mid - lx + 1 << 1);
+        node ret;
+        if (r < mid + 1) {
+            ret = query(left, lx, mid, l, r);
+        }
+        else if (mid < l) {
+            ret = query(right, mid + 1, rx, l, r);
+        }
+        else {
+            ret = merge(query(left, lx, mid, l, r), query(right, mid + 1, rx, l, r));
+        }
+        return ret;
     }
 
-    class proxy
-    {
-    public:
-        ll ind;
-        segment_tree *st;
-        proxy(segment_tree *a = 0, ll i = 0)
-        {
-            st = a;
-            ind = i;
+    int find_first(int x, int lx, int rx, int l, int r, const function<bool(node)> &F) {
+        if (rx < l || r < lx || !F(seg[x])) {
+            return -1;
         }
-        proxy &operator=(ll val)
-        {
-            st->update(ind, ind, val);
-            st->buildseg[ind] = val;
-            return *this;
+
+        if (lx == rx) {
+            return lx;
         }
-        operator T()
-        {
-            return st->buildseg[ind];
+
+        propagate(x, lx, rx);
+        int mid = lx + rx >> 1;
+        int left = x + 1;
+        int right = x + (mid - lx + 1 << 1);
+
+        int lft = find_first(left, lx, mid, l, r, F);
+        if (~lft) {
+            return lft;
         }
-    };
+        return find_first(right, mid + 1, rx, l, r, F);
+    }
 
 public:
-    segment_tree(vector<T> a)
-    {
-        buildseg = a;
-        while (size < a.size())
-            size *= 2;
-        seg.assign(2 * size, node());
-        build(0, 0, size - 1);
+    template <class... M>
+    segment_tree(int n, M... x) {
+        size = n;
+        seg.resize(2 * size - 1);
+        build(0, 0, size - 1, vector<M>(n, x)...);
     }
 
-    void update(ll ind, T value)
-    {
-        buildseg[ind] = value;
-        update(ind, value, 0, 0, size - 1);
+    template <class... M>
+    segment_tree(const vector<M> &...build_seg) {
+        size = (build_seg.size(), ...);
+        seg.resize(2 * size - 1);
+        build(0, 0, size - 1, build_seg...);
     }
 
-    proxy operator[](ll ind)
-    {
-        return proxy(this, ind);
+    template <class... M>
+    void update(int l, int r, const M &...value) {
+        update(0, 0, size - 1, l, r, value...);
     }
 
-    node query(ll l, ll r)
-    {
-        return query(l, r, 0, 0, size - 1);
+    node query(int l, int r) {
+        return query(0, 0, size - 1, l, r);
+    }
+
+    int find_first(int l, int r, const function<bool(node)> &F) {
+        return find_first(0, 0, size - 1, l, r, F);
     }
 };
 
-void Main()
-{
+void Main() {
+    
 }
 /*
 
@@ -135,18 +146,13 @@ void Main()
 
 
 
+
 */
-signed main()
-{
+signed main() {
     ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
     ll T = 1;
     // cin >> T;
-    for (ll i = 1; i <= T; i++)
-    {
+    for (ll i = 1; i <= T; i++) {
         Main();
         cout << '\n';
     }
