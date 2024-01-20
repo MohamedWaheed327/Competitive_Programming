557d8e2c47e04cbfcc2012da13814383f5710a31
MohamedWaheed327 <145778227+MohamedWaheed327@users.noreply.github.com>
2024-01-21 00:57:58 +0200

    Update MO.cpp

--- MO.cpp
+++ MO.cpp
@@ -3,102 +3,94 @@
 #define ll long long
 using namespace std;
 
-class MO // 1 index
-{
+class MO {
 private:
-    struct query
-    {
-        ll l, r, q_indx, b_indx;
+    struct query {
+        int l, r, q_indx, b_indx;
         query() {}
-        query(ll l, ll r, ll q_indx, ll b_indx)
-        {
+        query(int l, int r, int q_indx, int b_indx) {
             this->l = l;
             this->r = r;
             this->q_indx = q_indx;
             this->b_indx = b_indx;
         }
-        bool operator<(query x)
-        {
-            if (b_indx != x.b_indx)
-                return b_indx < x.b_indx;
-            return r < x.r;
-        }
     };
 
-    ll q = 0, SQ;
+    int q = 0, SQ;
     vector<ll> v;
     vector<query> Q;
 
     ll res = 0;
-    vector<ll> freq = vector<ll>(1e6 + 1, 0);
-    void add(ll ind)
-    {
+    int freq[100] = {};
+
+    void add(ll ind) {
         if (!freq[v[ind]])
             res++;
         freq[v[ind]]++;
     }
-    void remove(ll ind)
-    {
+
+    void remove(ll ind) {
         freq[v[ind]]--;
         if (!freq[v[ind]])
             res--;
     }
 
 public:
-    MO(vector<ll> a)
-    {
-        v = a;
-        SQ = sqrt(a.size());
+    template <class M>
+    MO(vector<M> v) {
+        this->v = v;
+        SQ = sqrt(v.size());
     }
 
-    void add_query(ll l, ll r)
-    {
-        Q.push_back(query(--l, --r, q++, l / SQ));
+    template <class... M>
+    void add_query(int l, int r, M... x) {
+        Q.push_back(query(l, r, q++, l / SQ, x...));
     }
 
-    void mo_process()
-    {
-        vector<ll> ans(q, 0);
+    void mo_process() {
+        vector<ll> ans(q);
 
-        sort(Q.begin(), Q.end());
-        ll r = 0, l = 1;
-        for (ll i = 0; i < q; i++)
-        {
-            while (r > Q[i].r)
+        sort(Q.begin(), Q.end(), [&](query a, query b) {
+            if (a.b_indx != b.b_indx)
+                return a.b_indx < b.b_indx;
+            return a.r < b.r;
+        });
+
+        ll l = 1, r = 0;
+        for (auto it : Q) {
+            while (r > it.r)
                 remove(r--);
-            while (r < Q[i].r)
+            while (r < it.r)
                 add(++r);
-            while (l > Q[i].l)
+            while (l > it.l)
                 add(--l);
-            while (l < Q[i].l)
+            while (l < it.l)
                 remove(l++);
-            ans[Q[i].q_indx] = res;
+            ans[it.q_indx] = res;
         }
 
-        for (ll i = 0; i < q; i++)
-        {
-            cout << ans[i] << "\n "[i == q - 1];
+        for (auto it : ans) {
+            cout << it << '\n';
         }
     }
 };
 
-void Main()
-{
+void Main() {
     ll n;
     cin >> n;
     vector<ll> v(n);
-    for (auto &it : v)
+    for (auto &it : v) {
         cin >> it;
+    }
 
     MO m(v);
 
     ll q;
     cin >> q;
-    while (q--)
-    {
+    while (q--) {
         ll l, r;
         cin >> l >> r;
-        m.add_query(l, r); // number of distinct elements form l to r
+        m.add_query(--l, --r); // number of distinct elements form l to r
     }
     m.mo_process();
 }
@@ -107,18 +99,13 @@ void Main()
 
 
 
+
 */
-signed main()
-{
+signed main() {
     ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
     ll T = 1;
     // cin >> T;
-    for (ll i = 1; i <= T; i++)
-    {
+    for (ll i = 1; i <= T; i++) {
         Main();
         cout << '\n';
     }
