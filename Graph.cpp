e22e9a919d580e763c6c2fc1490fd108741e51e2
MohamedWaheed327 <145778227+MohamedWaheed327@users.noreply.github.com>
2024-01-21 01:50:10 +0200

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,46 +1,36 @@
-// Mohamed_Waheed_
+// Mohamed_Waheed
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
 
-class graph
-{
+class graph {
 private:
-    ll N;
-    vector<ll> vis;
-    vector<ll> comp;
-    vector<ll> is_cyclic;
-    vector<ll> num_of_edges;
-    vector<vector<pair<ll, ll>>> g;
+    int N;
+    vector<bool> vis;
+    vector<int> dsu_parent;
+    vector<vector<pair<int, long long>>> g;
 
-    void do_dfs(ll node)
-    {
+    void do_dfs(int node) {
         if (vis[node])
             return;
         vis[node] = 1;
-        for (auto it : g[node])
-        {
-            if (!vis[it.first])
-            {
+        for (auto it : g[node]) {
+            if (!vis[it.first]) {
                 do_dfs(it.first);
             }
         }
     }
 
-    void bfs(vector<ll> parent, ll dis, vector<ll> &mn_dis)
-    {
+    void bfs(vector<int> parent, int dis, vector<int> &mn_dis) {
         if (parent.empty())
             return;
         for (auto it : parent)
             mn_dis[it] = dis;
-        vector<ll> child;
-        for (auto i : parent)
-        {
+        vector<int> child;
+        for (auto i : parent) {
             vis[i] = 1;
-            for (auto j : g[i])
-            {
-                if (!vis[j.first])
-                {
+            for (auto j : g[i]) {
+                if (!vis[j.first]) {
                     child.push_back(j.first);
                     vis[j.first] = 1;
                 }
@@ -49,115 +39,127 @@ private:
         bfs(child, dis + 1, mn_dis);
     }
 
-public:
-    graph(ll n)
-    {
-        N = n + 1;
-        g.resize(N);
-        num_of_edges.assign(N, 0);
+    int get_parent(int node) {
+        if (dsu_parent.empty()) {
+            dsu_parent.assign(N, 0);
+            iota(dsu_parent.begin(), dsu_parent.end(), 0);
+        }
+        if (node == dsu_parent[node])
+            return node;
+        return dsu_parent[node] = get_parent(dsu_parent[node]);
     }
-    ~graph() {}
 
-    void add_edge(ll u, ll v, ll cost = 0)
-    {
-        num_of_edges[u]++;
+public:
+    graph(int n) {
+        N = n;
+        g.resize(N);
+    }
+
+    void add_edge(int u, int v, long long cost = 0) {
+        --u, --v;
         g[u].push_back({v, cost});
     }
 
-    void dfs(ll node)
-    {
+    void dfs(int node) {
         vis.assign(N, 0);
         do_dfs(node);
     }
 
-    void components()
-    {
-        ll key = 0;
-        vis.assign(N, 0);
-        auto check = [&](auto self, ll node)
-        {
-            if (vis[node])
-                return;
-            comp[node] = key;
-            vis[node] = 1;
-            for (auto it : g[node])
-            {
-                if (!vis[it.first])
-                {
-                    self(self, it.first);
-                }
-            }
-        };
-        for (ll i = 0; i < N; i++)
-        {
-            if (!vis[i])
-            {
-                check(check, i);
-                key++;
-            }
-        }
-    }
-
-    vector<ll> is_on_cycle()
-    {
-        auto check = [&](auto self, ll node)
-        {
-            if (vis[node] || is_cyclic[node] != 1)
-                return;
-            is_cyclic[node]--;
-            vis[node] = 1;
-            for (auto it : g[node])
-            {
-                if (!vis[it.first])
-                {
-                    is_cyclic[it.first]--;
-                    self(self, it.first);
-                }
-            }
-        };
-        vis.assign(N, 0);
-        is_cyclic = num_of_edges;
-        for (ll i = 1; i < N; i++)
-            check(check, i);
-        return is_cyclic;
-    }
-
-    vector<ll> bfs(ll node)
-    {
-        vector<ll> dis(N, 1e18);
+    vector<int> bfs(int node) {
+        vector<int> dis(N, 1e9);
         vis.assign(N, 0);
         bfs({node}, 0, dis);
         return dis;
     }
 
-    vector<ll> dijkstra(ll start)
-    {
+    vector<int> components() {
+        int key = 1;
+        vector<int> comp(N);
+
+        auto check = [&](auto self, ll node) {
+            if (vis[node])
+                return;
+            comp[node] = key;
+            vis[node] = 1;
+            for (auto it : g[node]) {
+                if (!vis[it.first]) {
+                    self(self, it.first);
+                }
+            }
+        };
+
         vis.assign(N, 0);
-        priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
-        vector<ll> cheapest_path(N);
+        for (int i = 0; i < N; i++) {
+            if (!vis[i]) {
+                check(check, i);
+                key++;
+            }
+        }
+        return comp;
+    }
+
+    vector<int> is_on_cycle() {
+        vector<int> is_cyclic(N);
+        for (int i = 0; i < N; ++i) {
+            is_cyclic[i] = g[i].size();
+        }
+
+        auto check = [&](auto self, int node) {
+            if (vis[node] || is_cyclic[node] != 1)
+                return;
+            is_cyclic[node]--;
+            vis[node] = 1;
+            for (auto it : g[node]) {
+                if (!vis[it.first]) {
+                    is_cyclic[it.first]--;
+                    self(self, it.first);
+                }
+            }
+        };
+
+        vis.assign(N, 0);
+        for (int i = 0; i < N; i++) {
+            check(check, i);
+        }
+        return is_cyclic;
+    }
+
+    vector<long long> dijkstra(int start) {
+        vector<long long> cheapest_path(N, 1e18);
+        vis.assign(N, 0);
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
         pq.push({0, start});
-        while (pq.size())
-        {
-            ll cost = pq.top().first, node = pq.top().second;
+        while (pq.size()) {
+            auto [cost, node] = pq.top();
             pq.pop();
             if (vis[node])
                 continue;
             vis[node] = 1;
             cheapest_path[node] = cost;
-            for (auto [a, b] : g[node])
-            {
-                if (!vis[a])
-                {
-                    pq.push({b + cost, a});
+            for (auto [next, c] : g[node]) {
+                if (!vis[next]) {
+                    pq.push({cost + c, next});
                 }
             }
         }
         return cheapest_path;
     }
+
+    bool same_set(int u, int v) {
+        return get_parent(u) == get_parent(v);
+    }
+
+    void unite(int u, int v) {
+        u = get_parent(u);
+        v = get_parent(v);
+        dsu_parent[u] = v;
+    }
+
+    void solve() {
+    }
 };
 
-void Main()
-{
+void Main() {
 }
 /*
 
@@ -168,18 +170,13 @@ void Main()
 
 
 
+
 */
-signed main()
-{
+signed main() {
     ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
     ll T = 1;
     // cin >> T;
-    for (ll i = 1; i <= T; i++)
-    {
+    for (ll i = 1; i <= T; i++) {
         Main();
         cout << '\n';
     }
