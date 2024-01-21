78ddd4e61a98f4cf6c4a3427b8fdbdd1d05bcf4a
MohamedWaheed327 <145778227+MohamedWaheed327@users.noreply.github.com>
2024-01-21 02:14:09 +0200

    Update LIS.cpp

--- LIS.cpp
+++ LIS.cpp
@@ -3,143 +3,108 @@
 #define ll long long
 using namespace std;
 
-template <class T>
-class segment_tree
-{
+class segment_tree {
 private:
-    struct node
-    {
-        ll skip = 0;
-        node() { skip = 1; }
+    struct node {
+        long long mx;
 
-        ll mx;
-        node(T x)
-        {
-            mx = x;
+        void apply(int lx, int rx, long long val) {
+            mx = val;
         }
     };
 
-    ll size = 1;
-    vector<node> seg;
-    vector<T> buildseg;
-
-    node merge(node a, node b)
-    {
-        if (a.skip)
-            return b;
-        if (b.skip)
-            return a;
+    node merge(const node &a, const node &b) const {
         node ret;
-        ret.skip = 0;
         ret.mx = max(a.mx, b.mx);
         return ret;
     }
 
-    void build(ll x, ll lx, ll rx)
-    {
-        if (lx == rx)
-        {
-            if (lx < buildseg.size())
-                seg[x] = node(buildseg[lx]);
+    int size;
+    vector<node> seg;
+
+    template <class... M>
+    void build(int x, int lx, int rx, const vector<M> &...build_seg) {
+        if (lx == rx) {
+            seg[x].apply(lx, rx, build_seg[lx]...);
             return;
         }
-        ll m = (lx + rx) / 2;
-        build(2 * x + 1, lx, m);
-        build(2 * x + 2, m + 1, rx);
-        seg[x] = merge(seg[2 * x + 1], seg[2 * x + 2]);
+        int mid = lx + rx >> 1;
+        int left = x + 1;
+        int right = x + (mid - lx + 1 << 1);
+        build(left, lx, mid, build_seg...);
+        build(right, mid + 1, rx, build_seg...);
+        seg[x] = merge(seg[left], seg[right]);
     }
 
-    void update(ll ind, T value, ll x, ll lx, ll rx)
-    {
-        if (lx == rx)
-        {
-            seg[x] = node(value);
+    template <class... M>
+    void update(int x, int lx, int rx, int l, int r, const M &...value) {
+        if (l <= lx && rx <= r) {
+            seg[x].apply(lx, rx, value...);
             return;
         }
-        ll m = (lx + rx) / 2;
-        if (ind <= m)
-            update(ind, value, 2 * x + 1, lx, m);
-        else
-            update(ind, value, 2 * x + 2, m + 1, rx);
-        seg[x] = merge(seg[2 * x + 1], seg[2 * x + 2]);
+        int mid = lx + rx >> 1;
+        int left = x + 1;
+        int right = x + (mid - lx + 1 << 1);
+        if (l <= mid) {
+            update(left, lx, mid, l, r, value...);
+        }
+        if (r >= mid + 1) {
+            update(right, mid + 1, rx, l, r, value...);
+        }
+        seg[x] = merge(seg[left], seg[right]);
     }
 
-    node query(ll l, ll r, ll x, ll lx, ll rx)
-    {
-        if (l > rx || r < lx)
-            return node();
+    node query(int x, int lx, int rx, int l, int r) {
         if (l <= lx && rx <= r)
             return seg[x];
-        ll m = (lx + rx) / 2;
-        node s1 = query(l, r, 2 * x + 1, lx, m);
-        node s2 = query(l, r, 2 * x + 2, m + 1, rx);
-        return merge(s1, s2);
+        int mid = lx + rx >> 1;
+        int left = x + 1;
+        int right = x + (mid - lx + 1 << 1);
+        node ret;
+        if (r < mid + 1) {
+            ret = query(left, lx, mid, l, r);
+        }
+        else if (mid < l) {
+            ret = query(right, mid + 1, rx, l, r);
+        }
+        else {
+            ret = merge(query(left, lx, mid, l, r), query(right, mid + 1, rx, l, r));
+        }
+        return ret;
     }
-
-    class proxy
-    {
-    public:
-        ll ind;
-        segment_tree *st;
-        proxy(segment_tree *a = 0, ll i = 0)
-        {
-            st = a;
-            ind = i;
-        }
-        proxy &operator=(ll val)
-        {
-            st->update(ind, val);
-            st->buildseg[ind] = val;
-            return *this;
-        }
-        operator T()
-        {
-            return st->buildseg[ind];
-        }
-    };
 
 public:
-    segment_tree(vector<T> a)
-    {
-        buildseg = a;
-        while (size < a.size())
-            size *= 2;
-        seg.assign(2 * size, node());
-        build(0, 0, size - 1);
+    template <class... M>
+    segment_tree(const vector<M> &...build_seg) {
+        size = (build_seg.size(), ...);
+        seg.resize(2 * size - 1);
+        build(0, 0, size - 1, build_seg...);
     }
 
-    void update(ll ind, T value)
-    {
-        buildseg[ind] = value;
-        update(ind, value, 0, 0, size - 1);
+    template <class... M>
+    void update(int l, int r, const M &...value) {
+        update(0, 0, size - 1, l, r, value...);
     }
 
-    proxy operator[](ll ind)
-    {
-        return proxy(this, ind);
-    }
-
-    ll query(ll l, ll r)
-    {
-        if(l > r) return 0;
-        return query(l, r, 0, 0, size - 1).mx;
+    long long query(int l, int r) {
+        if (l > r) {
+            return 0;
+        }
+        return query(0, 0, size - 1, l, r).mx;
     }
 };
 
-vector<ll> compress(vector<ll> &v)
-{
+vector<ll> compress(vector<ll> &v) {
     ll n = v.size();
     vector<pair<ll, ll>> p;
-    for (ll i = 0; i < n; i++)
-    {
+    for (ll i = 0; i < n; i++) {
         p.push_back({v[i], i});
     }
     sort(p.begin(), p.end());
 
     vector<ll> real(n);
     ll last = p[0].first, nxt = 0;
-    for (auto [F, S] : p)
-    {
+    for (auto [F, S] : p) {
         if (last < F)
             nxt++;
         last = F;
@@ -149,37 +114,29 @@ vector<ll> compress(vector<ll> &v)
     return real;
 }
 
-vector<ll> lis(vector<ll> v)
-{
+vector<ll> lis(vector<ll> v) {
     auto real = compress(v);
     ll n = v.size();
     vector<ll> dp(n, 0);
     segment_tree st(dp);
+    vector<pair<ll, ll>> history;
 
-    struct x {  ll idx, old; };
-    vector<x> history;
-
-    for (auto it : v)
-    {
-        x temp;
-        temp.idx = it;
-        temp.old = dp[it];
-        history.push_back(temp);
+    for (auto it : v) {
+        history.push_back({it, dp[it]});
         dp[it] = max(dp[it], st.query(0, it - 1) + 1);
-        st.update(it, dp[it]);
+        st.update(it, it, dp[it]);
     }
 
-    ll mx = *max_element(dp.begin(), dp.end()), last = LLONG_MAX;
+    ll mx = st.query(0, n - 1), last = LLONG_MAX;
     vector<ll> ret;
-    for (ll i = n - 1; i >= 0; i--)
-    {
-        if (mx == dp[v[i]] && last > v[i])
-        {
+    for (ll i = n - 1; i >= 0; i--) {
+        if (mx == dp[v[i]] && last > v[i]) {
             mx--;
             last = v[i];
             ret.push_back(v[i]);
         }
-        dp[history.back().idx] = history.back().old;
+        auto [idx, val] = history.back();
+        dp[idx] = val;
         history.pop_back();
     }
     reverse(ret.begin(), ret.end());
@@ -188,8 +145,10 @@ vector<ll> lis(vector<ll> v)
     return ret;
 }
 
-void Main()
-{
+void Main() {
+    for (auto it : lis({2, 1, 5, 2, 8, 9, 4, 10})) {
+        cout << it << " ";
+    }
 }
 /*
 
@@ -200,17 +159,11 @@ void Main()
 
 
 */
-signed main()
-{
+signed main() {
     ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
     ll T = 1;
     // cin >> T;
-    for (ll i = 1; i <= T; i++)
-    {
+    for (ll i = 1; i <= T; i++) {
         Main();
         cout << '\n';
     }
