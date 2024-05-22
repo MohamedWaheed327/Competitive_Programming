d25d63c4c0b9b242fedae00c897676d472e65eb6
MohamedWaheed327 <145778227+MohamedWaheed327@users.noreply.github.com>
2024-05-22 19:15:49 +0300

    Update Sqrt_Decompostion.cpp

--- Sqrt_Decompostion.cpp
+++ Sqrt_Decompostion.cpp
@@ -1,32 +1,31 @@
-// Mohamed_Waheed_
+// Mohamed_Waheed
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
+template <class M>
 class SQblocks {
 private:
-    ll ans(const vector<ll> &blk) {
-        return blk[0];
-    }
-
     int SQ;
-    vector<ll> v;
-    vector<vector<ll>> block;
+    vector<M> v;
+    vector<long long> ans;
+    vector<vector<long long>> block;
 
 public:
-    SQblocks(const vector<ll> &v) {
+    SQblocks(const vector<M> &v) {
         SQ = sqrt(v.size()) + 1;
         this->v = v;
+        ans.resize(SQ);
         block.resize(SQ);
         for (int i = 0; i < v.size(); i++) {
             block[i / SQ].push_back(v[i]);
         }
-        for (auto &it : block) {
-            sort(it.begin(), it.end());
+        for (int i = 0; i < SQ; ++i) {
+            sort(block[i].begin(), block[i].end());
+            if (block[i].size()) ans[i] = block[i][0];
         }
     }
 
-    void update(int ind, ll val) {
+    void update(int ind, long long val) {
         auto &blk = block[ind / SQ];
         int it = lower_bound(blk.begin(), blk.end(), v[ind]) - blk.begin();
         v[ind] = blk[it] = val;
@@ -39,17 +38,18 @@ public:
             swap(blk[it], blk[it + 1]);
             ++it;
         }
+        ans[ind / SQ] = blk[0];
     }
 
-    ll query(int l, int r) {
-        ll ret = LLONG_MAX;
+    long long query(int l, int r) {
+        long long ret = LLONG_MAX;
         while (l <= r) {
             if (l % SQ || r - l < SQ) {
-                ret = min(ret, ans({v[l]}));
+                ret = min(ret, 1ll * v[l]);
                 l += 1;
             }
             else {
-                ret = min(ret, ans(block[l / SQ]));
+                ret = min(ret, ans[l / SQ]);
                 l += SQ;
             }
         }
@@ -57,28 +57,26 @@ public:
     }
 };
 
-void Main() {
+void Main(...) {
     int n, q;
     cin >> n >> q;
 
-    vector<ll> v(n);
-    for (ll i = 0; i < n; ++i) {
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
         cin >> v[i];
     }
 
     SQblocks sq(v);
     while (q--) {
-        ll op;
+        int op, ind, v, l, r;
         cin >> op;
         if (op == 1) {
-            ll ind, v;
             cin >> ind >> v;
-            sq.update(ind, v);
+            sq.update(--ind, v);
         }
         else {
-            ll l, r;
             cin >> l >> r;
-            cout << sq.query(l, --r) << '\n';
+            cout << sq.query(--l, --r) << '\n';
         }
     }
 }
@@ -88,14 +86,16 @@ void Main() {
 
 
 
+
+
+
+
 */
 signed main() {
-    ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
-    ll T = 1;
+    cin.tie(0)->sync_with_stdio(0);
+    int T = 1;
     // cin >> T;
-    for (ll i = 1; i <= T; i++) {
-        Main();
-        cout << '\n';
+    for (int i = 1; i <= T; i++) {
+        Main(i), cout << '\n';
     }
-    return 0;
 }
