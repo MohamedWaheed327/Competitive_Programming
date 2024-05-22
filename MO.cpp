7013c7853ff6bab2e8fe023bbe86132cc1bd12f7
MohamedWaheed327 <145778227+MohamedWaheed327@users.noreply.github.com>
2024-05-22 18:57:03 +0300

    Update MO.cpp

--- MO.cpp
+++ MO.cpp
@@ -1,42 +1,40 @@
-// Mohamed_Waheed_
+// Mohamed_Waheed
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
+template <class M>
 class MO {
 private:
     struct query {
         int l, r, q_indx;
     };
 
-    int q = 0, SQ;
-    vector<long long> v;
+    int q, SQ;
+    vector<M> v;
     vector<query> Q;
 
-    ll res = 0;
-    int freq[100] = {};
-
+    long long res = 0;
     void add(int ind) {
-        if (!freq[v[ind]])
-            res++;
-        freq[v[ind]]++;
+        res += v[ind];
     }
 
     void remove(int ind) {
-        freq[v[ind]]--;
-        if (!freq[v[ind]])
-            res--;
+        res -= v[ind];
+    }
+
+    long long calc(...) {
+        return res;
     }
 
 public:
-    template <class M>
     MO(const vector<M> &v) {
+        q = 0;
         this->v = v;
         SQ = sqrt(v.size());
     }
 
-    template <class... M>
-    void add_query(int l, int r, M &...x) {
+    template <class... T>
+    void add_query(int l, int r, T &...x) {
         Q.push_back({l, r, q++, x...});
     }
 
@@ -44,9 +42,7 @@ public:
         vector<long long> ans(q);
 
         sort(Q.begin(), Q.end(), [&](query a, query b) {
-            if (a.l / SQ != b.l / SQ)
-                return a.l / SQ < b.l / SQ;
-            return a.r < b.r;
+            return pair{a.l / SQ, a.r} < pair{b.l / SQ, b.r};
         });
 
         int l = 1, r = 0;
@@ -59,7 +55,7 @@ public:
                 add(--l);
             while (l < L)
                 remove(l++);
-            ans[q_indx] = res;
+            ans[q_indx] = calc();
         }
 
         for (auto it : ans) {
@@ -68,24 +64,8 @@ public:
     }
 };
 
-void Main() {
-    ll n;
-    cin >> n;
-    vector<ll> v(n);
-    for (auto &it : v) {
-        cin >> it;
-    }
-
-    MO m(v);
-
-    ll q;
-    cin >> q;
-    while (q--) {
-        ll l, r;
-        cin >> l >> r;
-        m.add_query(--l, --r); // number of distinct elements form l to r
-    }
-    m.mo_process();
+void Main(...) {
+    
 }
 /*
 
@@ -93,14 +73,16 @@ void Main() {
 
 
 
+
+
+
+
 */
 signed main() {
-    ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
-    ll T = 1;
+    cin.tie(0)->sync_with_stdio(0);
+    int T = 1;
     // cin >> T;
-    for (ll i = 1; i <= T; i++) {
-        Main();
-        cout << '\n';
+    for (int i = 1; i <= T; i++) {
+        Main(i), cout << '\n';
     }
-    return 0;
 }
