1cd816e9f654f4e7ee4fa7b47a7f39e5980bcc34
MohamedWaheed327 <145778227+MohamedWaheed327@users.noreply.github.com>
2024-09-16 18:39:10 +0300

    Update hashing(with update).cpp

--- hashing(with update).cpp
+++ hashing(with update).cpp
@@ -2,61 +2,101 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const long long mod1 = 1e9 + 7, mod2 = 2e9 + 11, base1 = 31, base2 = 37;
-vector<long long> pow1, pow2;
-signed init(int N = 1e6) {
-    pow1.resize(N + 1);
-    pow2.resize(N + 1);
-    pow1[0] = pow2[0] = 1;
-    for (int i = 1; i <= N; i++) {
-        pow1[i] = (base1 * pow1[i - 1]) % mod1;
-        pow2[i] = (base2 * pow2[i - 1]) % mod2;
+const long long mod1 = 1e9 + 7, mod2 = 2e9 + 11, base1 = 31, base2 = 327;
+vector<int> _pow1{1}, _pow2{1}, _powsum1{0}, _powsum2{0};
+void init(int N) {
+    while (_pow1.size() <= N) {
+        _powsum1.push_back((0LL + _pow1.back() + _powsum1.back()) % mod1);
+        _pow1.push_back(1LL * _pow1.back() * base1 % mod1);
+        _powsum2.push_back((0LL + _pow2.back() + _powsum2.back()) % mod2);
+        _pow2.push_back(1LL * _pow2.back() * base2 % mod2);
     }
-    return 0;
 }
-int DO_It = init();
+long long pow1(int p) {
+    init(p);
+    return _pow1[p];
+}
+long long pow2(int p) {
+    init(p);
+    return _pow2[p];
+}
+long long powsum1(int p) {
+    init(p);
+    return _powsum1[p];
+}
+long long powsum2(int p) {
+    init(p);
+    return _powsum2[p];
+}
 
 class dr_string {
 private:
     class segment_tree {
     private:
         struct node {
-            int len, h1, h2;
+            int len, h1, h2, lazy = -1;
 
-            void apply(int lx, int rx, long long val) {
+            void apply(int lx, int rx, long long val = 0) {
+                lazy = val;
                 len = rx - lx + 1;
-                h1 = (val * pow1[1]) % mod1;
-                h2 = (val * pow2[1]) % mod2;
+                h1 = val * powsum1(len) % mod1;
+                h2 = val * powsum2(len) % mod2;
             }
 
             void merge(const node &a, const node &b) {
                 len = a.len + b.len;
-                h1 = (a.h1 + b.h1 * pow1[a.len]) % mod1;
-                h2 = (a.h2 + b.h2 * pow2[a.len]) % mod2;
+                h1 = (a.h1 + b.h1 * pow1(a.len)) % mod1;
+                h2 = (a.h2 + b.h2 * pow2(a.len)) % mod2;
             }
 
             bool operator==(const node &x) {
-                return (h1 == x.h1 && h2 == x.h2);
+                return h1 == x.h1 && h2 == x.h2;
             }
         };
+
+        inline void propagate(int x, int lx, int rx) {
+            int mid = lx + rx >> 1;
+            int left = x + 1;
+            int right = x + (mid - lx + 1 << 1);
+            if (~seg[x].lazy) {
+                seg[left].apply(lx, mid, seg[x].lazy);
+                seg[right].apply(mid + 1, rx, seg[x].lazy);
+                seg[x].lazy = -1;
+            }
+        }
 
         int size;
         vector<node> seg;
 
-        template <class M>
-        void update(int x, int lx, int rx, int l, int r, M value) {
-            if (l <= lx && rx <= r) {
-                seg[x].apply(lx, rx, value);
+        template <class... M>
+        void build(int x, int lx, int rx, const vector<M> &...build_seg) {
+            if (lx == rx) {
+                seg[x].apply(lx, rx, build_seg[lx]...);
                 return;
             }
             int mid = lx + rx >> 1;
             int left = x + 1;
             int right = x + (mid - lx + 1 << 1);
+            build(left, lx, mid, build_seg...);
+            build(right, mid + 1, rx, build_seg...);
+            seg[x].merge(seg[left], seg[right]);
+        }
+
+        template <class... M>
+        void update(int x, int lx, int rx, int l, int r, const M &...value) {
+            if (l <= lx && rx <= r) {
+                seg[x].apply(lx, rx, value...);
+                return;
+            }
+            propagate(x, lx, rx);
+            int mid = lx + rx >> 1;
+            int left = x + 1;
+            int right = x + (mid - lx + 1 << 1);
             if (l <= mid) {
-                update(left, lx, mid, l, r, value);
+                update(left, lx, mid, l, r, value...);
             }
             if (r >= mid + 1) {
-                update(right, mid + 1, rx, l, r, value);
+                update(right, mid + 1, rx, l, r, value...);
             }
             seg[x].merge(seg[left], seg[right]);
         }
@@ -65,6 +105,7 @@ private:
             if (l <= lx && rx <= r) {
                 return seg[x];
             }
+            propagate(x, lx, rx);
             int mid = lx + rx >> 1;
             int left = x + 1;
             int right = x + (mid - lx + 1 << 1);
@@ -82,14 +123,18 @@ private:
         }
 
     public:
-        segment_tree(int n = 1) {
-            size = n;
+        segment_tree() {}
+
+        template <class... M>
+        segment_tree(const vector<M> &...build_seg) {
+            size = (build_seg.size(), ...);
             seg.resize(2 * size - 1);
+            build(0, 0, size - 1, build_seg...);
         }
 
-        template <class M>
-        void update(int ind, M value) {
-            update(0, 0, size - 1, ind, ind, value);
+        template <class... M>
+        void update(int l, int r, const M &...value) {
+            update(0, 0, size - 1, l, r, value...);
         }
 
         node query(int l, int r) {
@@ -101,42 +146,39 @@ private:
     segment_tree A, B;
 
 public:
-    dr_string(int n = 1) {
-        size = n;
-        A = segment_tree(n);
-        B = segment_tree(n);
-    }
-
     dr_string(string s) {
-        int n = s.size();
-        size = n;
-        A = segment_tree(n);
-        B = segment_tree(n);
-        for (int i = 0; i < n; ++i) {
-            update(i, s[i]);
-        }
+        size = s.size();
+        A = segment_tree(vector<char>(s.begin(), s.end()));
+        reverse(s.begin(), s.end());
+        B = segment_tree(vector<char>(s.begin(), s.end()));
     }
 
-    void update(int ind, char val) {
-        A.update(ind, val - 'a');
-        B.update(size - ind - 1, val - 'a');
+    void update(int l, int r, char val) {
+        A.update(l, r, val);
+        B.update(size - r - 1, size - l - 1, val);
     }
 
     auto substr(int l, int r) {
         return A.query(l, r);
     }
 
-    bool is_palindrome(int l, int r) {
-        return A.query(l, r) == B.query(size - r - 1, size - l - 1);
+    auto substr_rev(int l, int r) {
+        return B.query(size - r - 1, size - l - 1);
     }
 
-    bool operator==(dr_string s) {
-        return A.query(0, size - 1) == s.A.query(0, size - 1);
+    bool is_palindrome(int l, int r) {
+        return substr(l, r) == substr_rev(l, r);
     }
 };
 
 void Main(...) {
-    
+    int n;
+    string s;
+    cin >> n >> s;
+
+    dr_string h(s);
+    h.update(0, 0, 'x');
+    cout << h.is_palindrome(0, n - 1);
 }
 /*
 
