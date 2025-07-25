5332bab617e03296ea9693cb6af54b43513959ee
MohamedWaheed327 <145778227+MohamedWaheed327@users.noreply.github.com>
2025-07-25 07:38:23 +0300

    Create bfs.cpp

---
+++ graphs/bfs.cpp
@@ -0,0 +1,64 @@
+// Mohamed_Waheed
+#include <bits/stdc++.h>
+using namespace std;
+
+template <int base = 1>
+class BFS {
+private:
+    vector<int> par;
+
+public:
+    vector<int64_t> dis;
+    const int64_t INF = 1e18;
+
+    BFS(vector<vector<int>> &g, vector<int> src = {base}) : dis(g.size(), INF), par(g.size(), -1) {
+        queue<int> q;
+        for (auto it : src) {
+            dis[it] = 0;
+            q.push(it);
+        }
+        while (q.size()) {
+            int node = q.front();
+            q.pop();
+            for (auto it : g[node]) {
+                if (dis[it] == INF) {
+                    par[it] = node;
+                    dis[it] = dis[node] + 1;
+                    q.push(it);
+                }
+            }
+        }
+    }
+
+    vector<int> get_path(int node) {
+        if (dis[node] == INF) return {};
+        vector<int> ret;
+        for (int cur = node; ~cur; cur = par[cur]) {
+            ret.push_back(cur);
+        }
+        return vector<int>(ret.rbegin(), ret.rend());
+    }
+};
+
+void Main(...) {
+    
+}
+/*
+
+
+
+
+
+
+
+
+
+*/
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int T = 1;
+    // cin >> T;
+    for (int i = 1; i <= T; i++) {
+        Main(i), cout << '\n';
+    }
+}
